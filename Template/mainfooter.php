c776346625a6a47c1e7e556ce768e1a44b3b3ae4
Ridz-sensei <146825139+Ridz-sensei@users.noreply.github.com>
2024-12-05 10:54:53 +0700

    pisah template

---
+++ Template/mainfooter.php
@@ -0,0 +1,9 @@
+<div class="footer text-center m-3">
+        <div class="card rounded-pill">
+            @Care Rythm
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
